bec8aa7a683bb7bfb0926cb0f541db51c9b496bc
Karoid <shj5508@naver.com>
2017-03-20 13:56:04 +0900

    profile changed, admin can now use iframe

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -22,6 +22,11 @@ class BoardController < ApplicationController
       end
       @article.title = params[:title]
       @article.content = Loofah.scrub_fragment(params[:content], :escape)
+      if current_member
+        if current_member.admin
+          @article.content = params[:content]
+        end
+      end
       @article.member_id = current_member ? current_member.id : 0
       @article.member_name = current_member ? "#{current_member.senior_number}기 #{current_member.username}" : "비회원"
       @article.active = true
