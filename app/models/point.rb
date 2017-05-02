6b773dbd5ab6bd02557dc2a9ea47f92474cb2b04
Karoid <shj5508@naver.com>
2017-05-02 15:05:45 +0900

    rcp function added

--- app/models/point.rb
+++ app/models/point.rb
@@ -2,6 +2,7 @@ class Point < ActiveRecord::Base
 end
 require 'article'
 require 'statistic'
+require 'wiki_page_version'
 class Statistic
   after_save :give_point
   #저장할때 포인트를 줍니다.
@@ -11,10 +12,10 @@ class Statistic
     when "write_problem"
       article = self.target_model.camelize.constantize.find(self.target_id.to_i)
       if point
-        point.point += 3000
+        point.point += 1500
         point.save
       else
-        Point.create(user_id: self.member_id, point: 8000)
+        Point.create(user_id: self.member_id, point: 6500)
       end
     when "sign_in"
       if point
@@ -32,6 +33,18 @@ class Article
   def statistic
     if self.board.route == "problems"
       Statistic.create(name: "write_problem", member_id: self.member_id, target_model: "Board", target_id: self.board.id)
+    end
+  end
+end
+class WikiPageVersion
+  after_save :add_after_commit
+  def add_after_commit
+    point = Point.where(user_id: self.updator_id).take
+    if point
+      point.point += 500
+      point.save
+    else
+      Point.create(user_id: self.updator_id, point: 5500)
     end
   end
 end
