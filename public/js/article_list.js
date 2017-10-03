ce646181114ae3cefe351721b1ff7b698a1b27f6
Karoid <shj5508@naver.com>
2017-10-03 20:47:59 +0900

    wiki list double ajax error fixed

--- public/js/article_list.js
+++ public/js/article_list.js
@@ -42,41 +42,45 @@ false;
 var page = parseInt($("nav div em").html())
 
 $(document).scroll(function() {
-    if (Math.round($(document).scrollTop()) >= $("#article").height() - $(window).height()+89) {
-loading = $('<div id="loading" class="load" style="display: none;"><span><img src="http://www.downgraf.com/wp-content/uploads/2014/09/01-progress.gif" alt="cargando..."/></span></div>');
-$('table.article_list').after(loading);
-loading.fadeIn();
-$.ajax({
-  type: 'GET',
-  url: window.location.href + "?page="+(page+1),
-  dataType: 'json',
-  data: {json:true},
-  success: (function(data) {
-    return loading.fadeOut(function() {
-      var html = ""
-      $.each(data,function(index, article) {
-        var datetime = new Date(article.created_at)
-        console.log(datetime,datetime.getYear())
-        html += '<tr>'+
-        '<td class="td_number" scope="row">'+article.id+'</th>'+
-        '<td class="td_title"><a href="/wiki/'+window.location.pathname.split("/")[2]+'/'+window.location.pathname.split("/")[3]+'/'+article.id+'">'+article.title+'</a></td>'+
-        '<td class="td_name">'+article.member_name+'</td>'+
-        '<td class="td_date"><span class="hidden-xs">'+datetime.getFullYear()+'.</span>'+datetime.getMonth()+"."+datetime.getDate()+'</td>'+
-        '<td class="td_page_view">'+article.view+'</td>'+
-        '</tr>'
+  if (Math.round($(document).scrollTop()) >= $("#article").height() - $(window).height()+89 && loading_state == false) {
+  loading_state = true
+  loading = $('<div id="loading" class="load" style="display: none;"><span><img src="http://www.downgraf.com/wp-content/uploads/2014/09/01-progress.gif" alt="cargando..."/></span></div>');
+  $('table.article_list').after(loading);
+  loading.fadeIn();
+  $.ajax({
+    type: 'GET',
+    url: window.location.href + "?page="+(page+1),
+    dataType: 'json',
+    data: {json:true},
+    success: (function(data) {
+      return loading.fadeOut(function() {
+        var html = ""
+        $.each(data,function(index, article) {
+          var datetime = new Date(article.created_at)
+          console.log(datetime,datetime.getYear())
+          html += '<tr>'+
+          '<td class="td_number" scope="row">'+article.id+'</th>'+
+          '<td class="td_title"><a href="/wiki/'+window.location.pathname.split("/")[2]+'/'+window.location.pathname.split("/")[3]+'/'+article.id+'">'+article.title+'</a></td>'+
+          '<td class="td_name">'+article.member_name+'</td>'+
+          '<td class="td_date"><span class="hidden-xs">'+datetime.getFullYear()+'.</span>'+("0" + (datetime.getMonth() + 1)).slice(-2)+"."+("0" + datetime.getDate()).slice(-2)+'</td>'+
+          '<td class="td_page_view">'+article.view+'</td>'+
+          '</tr>'
+        });
+        $('table.article_list tbody').append(html);
+        $('.load').remove()
+        page+=1
+        return loading.remove();
       });
-      $('table.article_list tbody').append(html);
-      $('.load').remove()
-      page+=1
-      return loading.remove();
-    });
-  }),
-  error: function(data){
-    $('nav').show()
-    return loading.remove();
+    }),
+      error: function(err){
+        $('nav').show()
+        return loading.remove();
+      },
+      complete: function(){
+        loading_state = false
+      }
+  });
   }
-});
-}
 });
 
 //검색 상태 표시
