a95037dec76f41333081065c7f93c8c5058eeba2
Seoung Ho Jeong <shj5508@naver.com>
2017-10-11 09:32:22 +0000

    kakao plusfriend wiki make

--- app/controllers/kakao_chat_controller.rb
+++ app/controllers/kakao_chat_controller.rb
@@ -1,4 +1,6 @@
 class KakaoChatController < ApplicationController
+    @@presets = ["휴즈 위키 읽기","이미지 업로드하기", "공지 올리기"]
+    
     def keyboard
         render :json => {
             :type => "buttons",
@@ -11,42 +13,36 @@ class KakaoChatController < ApplicationController
         type = params[:type]
         message = params[:content]
         valid_email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-        login_data = KakaoChatLogin.where(user_key: user_key).take
-        @keyboard = {type: "text"}
+        @login_data = KakaoChatLogin.where(user_key: user_key).take
+        @data = {
+            :message => {
+                :text => ""
+            },
+            :keyboard => {type: "text"}
+        }
         
-        if login_data.nil?
-            res = "로그인이 필요합니다. 휴즈넷 이메일을 입력해주세요!"
+        if @login_data.nil?
+            @data[:message][:text] = "로그인이 필요합니다. 휴즈넷 이메일을 입력해주세요!"
             if message =~ valid_email_regex 
                 if mem = Member.where(email: message)[0]
                     KakaoChatLogin.find_or_create_by(user_key: user_key, member_id: mem.id)
-                    res = "휴즈넷에 로그인 하여 기기 #{user_key}를 허용해주세요\nhttp://huhs.net/accept_api"
-                    @keyboard[:type] = "buttons"
-                    @keyboard[:buttons] = ["인증 확인하기"]
+                    before_active_message
                 else
-                    res = "휴즈넷에 존재하지 않는 이메일입니다."
+                    @data[:message][:text] = "휴즈넷에 존재하지 않는 이메일입니다."
                 end
             end
             
         else
             
-            if !login_data.active
-                res = "휴즈넷에 로그인 하여 기기 #{user_key}를 허용해주세요\nhttp://huhs.net/accept_api"
-                @keyboard[:type] = "buttons"
-                @keyboard[:buttons] = ["인증 확인하기"]
+            if !@login_data.active
+                before_active_message
             else
-                res = "로그인 완료"
-                @keyboard[:type] = "buttons"
-                @keyboard[:buttons] = ["휴즈 위키 읽기","이미지 업로드하기", "공지 올리기"]
+                login_success
             end
         
         end
         
-        render :json => {
-            :message => {
-                :text => res
-            },
-            :keyboard => @keyboard
-        }
+        render :json => @data
     end
     
     def friend_add
@@ -76,6 +72,83 @@ class KakaoChatController < ApplicationController
             @kakao_chat_login.where(user_key: params[:user_key]).take.destroy
             render :json => {
                 :message => 'success'
+            }
+        end
+    end
+    
+    private
+    
+    def before_active_message
+        @data[:message][:text] = "휴즈넷에 로그인 하여 기기 #{params[:user_key]}를 허용해주세요\nhttp://huhs.net/accept_api"
+        @data[:keyboard][:type] = "buttons"
+        @data[:keyboard][:buttons] = ["인증 확인하기"]
+    end
+    
+    def login_success
+        case params[:content]
+        when @@presets[0]
+            #휴즈 위키 읽기
+            @data = {
+                message: {text: "읽고싶은 휴즈 위키의 글을 골라주세요!"},
+                keyboard: {
+                    type: 'buttons',
+                    buttons: WikiPage.limit(3).offset(0).map{|x| "|위키|"+x.title} + ['다음#1','처음으로']
+                }
+            }
+        when @@presets[1]
+            #이미지 업로드하기
+            @data = {
+                message: {text: "준비중입니다"},
+                keyboard: {
+                    type: 'buttons',
+                    buttons: ['처음으로']
+                }
+            }
+        when @@presets[2]
+            #공지 업로드하기
+            @data = {
+                message: {text: "준비중입니다"},
+                keyboard: {
+                    type: 'buttons',
+                    buttons: ['처음으로']
+                }
+            }
+        else
+            if params[:content] =~ /\|위키\|/
+                title = params[:content].split("|위키|")[1]
+                @data[:message][:text] = WikiPage.where(title: title)[0].content
+                @data[:keyboard] = {
+                    type: "buttons",
+                    buttons: @@presets
+                }
+            elsif params[:content] =~ /다음#/
+                page = params[:content].split("다음#")[1].to_i
+                @data = {
+                    message: {text: "위키 페이지: #{page}"},
+                    keyboard: {
+                        type: 'buttons',
+                        buttons: WikiPage.limit(3).offset(3*page).map{|x| "|위키|"+x.title} + ['다음#'+(page+1).to_s,'처음으로']
+                    }
+                }
+            else
+                login_state_message
+            end
+        end
+        
+    end
+    
+    def login_state_message
+        img = @login_data.member.image_url
+        @data[:message][:text] = "[[로그인 상태]]\nEmail: #{@login_data.member.email}\nName: #{@login_data.member.username}"
+        @data[:keyboard] = {
+            type: "buttons",
+            buttons: @@presets
+        }
+        if img[0] != '/'
+            @data[:message][:photo] = {
+                url: img,
+                width: 100,
+                height: 100
             }
         end
     end
