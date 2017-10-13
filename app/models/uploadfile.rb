3334ea4d988efb38fcc0fc460aeb44f0feb4e172
Seoung Ho Jeong <shj5508@naver.com>
2017-10-13 17:48:48 +0000

    destroy file optimized

--- app/models/uploadfile.rb
+++ app/models/uploadfile.rb
@@ -3,7 +3,9 @@ class Uploadfile < ActiveRecord::Base
   def self.destroy_files(article_id)
     destroy_files = Uploadfile.where(article_id: article_id)
     destroy_files.each do |file|
-      Cloudinary::Uploader.destroy(file.public_id, options = {})
+      Thread.new do
+        Cloudinary::Uploader.destroy(file.public_id, options = {})
+      end
     end
     destroy_files.destroy_all
   end
